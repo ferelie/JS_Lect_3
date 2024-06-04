646212fd864523248ecbe885a859d58ab46ecb1a
ferelie <abdallah.yasser0501@gmail.com>
2024-06-04 14:29:14 +0300

    modified (readability)

--- Task_2/script.js
+++ Task_2/script.js
@@ -12,13 +12,13 @@ let starWarsData = {};
 
 function displayData(data) {
     const table = document.querySelector("table");
-    for (let i = 0; i < data.length; i++) {
+    for (element of data) {
         const row = table.insertRow();
         const titleCell = row.insertCell();
         const episodeCell = row.insertCell();
         const releaseCell = row.insertCell();
-        titleCell.innerHTML = data[i].title;
-        episodeCell.innerHTML = data[i].episode_id;
-        releaseCell.innerHTML = data[i].release_date;
+        titleCell.innerText = element.title;
+        episodeCell.innerText = element.episode_id;
+        releaseCell.innerText = element.release_date;
     }
 }
